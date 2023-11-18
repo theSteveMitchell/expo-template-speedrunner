79c73f9b9893d86e234dee0408b61c7f7caedb19
Steve Mitchell <thestevemitchell@gmail.com>
2023-11-18 17:59:51 -0500

    eva colors and babel module resolver

--- App.js
+++ App.js
@@ -1,10 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
 import * as eva from '@eva-design/eva';
 import { ApplicationProvider, Layout, Text } from '@ui-kitten/components';
+import { default as theme } from 'config/theme.json';
 
 export default function App() {
   return (
-    <ApplicationProvider {...eva} theme={eva.light}>
+    <ApplicationProvider {...eva} theme={{ ...eva.light, ...theme }}>
       <Layout style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
         <Text>Open up App.js to start working on your app!</Text>
         <StatusBar style="auto" />
